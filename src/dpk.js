801c3332bd61e44344c8ff0ae36998f1d7c143e8
Michael Kim <mkim0407@gmail.com>
2022-11-19 21:49:57 -0800

    return early

--- src/dpk.js
+++ src/dpk.js
@@ -7,21 +7,21 @@ const getHash = (s) => {
 exports.deterministicPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
 
   if (!event) {
-    candidate = TRIVIAL_PARTITION_KEY;
-  } else if (event.partitionKey) {
-    candidate = event.partitionKey;
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-    if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-      candidate = getHash(candidate);
-    }
-  } else {
-    candidate = getHash(JSON.stringify(event));
+    return TRIVIAL_PARTITION_KEY;
   }
 
+  if (!event.partitionKey) {
+    return getHash(JSON.stringify(event))
+  }
+
+  let candidate = event.partitionKey;
+  if (typeof candidate !== "string") {
+    candidate = JSON.stringify(candidate);
+  }
+  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
+    candidate = getHash(candidate);
+  }
   return candidate;
 };
